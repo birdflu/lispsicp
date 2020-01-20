f69c0101b67dbd02538f30fdbcdc19edb69c3108
birdflu <birdflu@mail.ru>
2020-01-20 10:21:35 +0300

    Create exercise1.19.rkt

---
+++ Chapter1/exercise1.19.rkt
@@ -0,0 +1,23 @@
+#lang scheme
+
+;The clever algorithm for computing
+;the Fibonacci numbers in a logarithmic number of steps.
+
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   (+ (* p p) (* q q))
+                   (+ (* q q) (* 2 p q))
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
+
+(display (fib 10))
