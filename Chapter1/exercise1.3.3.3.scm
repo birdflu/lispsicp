6ac174f5943e8699de5f1fcce7b6bdbd102e8efb
birdflu <birdflu@mail.ru>
2020-02-06 04:04:35 +0300

    Update exercise1.3.3.3.scm

--- Chapter1/exercise1.3.3.3.scm
+++ Chapter1/exercise1.3.3.3.scm
@@ -1,13 +1,5 @@
 #lang scheme
 
-; A number x is called a fixed point of a function f if x satisfies the equation
-; f (x) = x. For some functions f we can locate a fixed point by
-; beginning with an initial guess and applying f repeatedly,
-; f(x), f(f(x)), f(f(f(x))), ...
-; until the value does not change very much. Using this idea, 
-; a procedure fixedPoint takes as inputs a function and an
-; initial guess and produces an approximation to a fixed point of the function.
-
 (define (halfInterval f left right)
   (let ((avg (/ (+  left right) 2))
         )
