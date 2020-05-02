0823a57bf678212e42ba94b7f39d030d27960944
birdflu <birdflu@mail.ru>
2020-05-02 22:54:46 +0300

    Create exercise2.2.1.1.scm

---
+++ Chapter2/exercise2.2.1.1.scm
@@ -0,0 +1,26 @@
+#lang scheme
+; The simple training for list 
+
+(define nil '())
+(define l1 (list 1 2 3 4 ))
+(define l2 (cons 1 (cons 2 (cons 3 ( cons 4 nil)))))
+(define message (list l1 'is 'equal l2))
+message
+
+(car l1) ; 1 expected
+(cdr l1) ; (2 3 4) expected
+(cons 0 l1) ; (0 1 2 3 4) expected
+
+(define (list-ref items n)
+  (if (= n 0)
+     (car items)
+     (list-ref (cdr items) (- n 1))))
+
+(list-ref l1 2)  ; 3 expected
+
+(define (length items)
+  (if (null? items)
+     0
+     (+ 1 (length (cdr items)))))
+
+(length l1)  ; 4 expected
