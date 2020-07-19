9b8a35f5e641cdb5b7ff13a91796038012f0076e
birdflu <birdflu@mail.ru>
2020-07-19 20:55:16 +0300

    Create exercise2.17.scm

---
+++ Chapter2/exercise2.17.scm
@@ -0,0 +1,12 @@
+#lang scheme
+
+; Define a procedure last-pair that returns
+; the list that contains only the last element of a given (nonempty)
+; list:
+
+(define (last-pair items)
+  (if (null? (cdr items))
+     items
+     (last-pair (cdr items))))
+
+(last-pair (list 23 72 149 34))
