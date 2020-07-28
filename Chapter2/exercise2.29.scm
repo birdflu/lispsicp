3fb9662cee1a2ee705f8f625a314a4545c6a03dc
birdflu <birdflu@mail.ru>
2020-07-28 12:00:55 +0300

    Update exercise2.29.scm

--- Chapter2/exercise2.29.scm
+++ Chapter2/exercise2.29.scm
@@ -22,7 +22,14 @@
    (make-branch 2 d)))
 
 (define a
-  (make-mobile (make-branch 9 8) (make-branch 10 b)))
+  (make-mobile
+   (make-branch 9 8)
+   (make-branch 10 b)))
+
+(define balance-mobile
+  (make-mobile
+   (make-branch 2 a)
+   (make-branch 2 a)))
 
 (define (left-branch mobile)
   (car mobile))
@@ -62,14 +69,44 @@
             (total-weight (branch-structure right-br))
             (branch-structure right-br))))))
 
+(define (rotational-moment mobile)
+  (let ((left-br (left-branch mobile))
+        (right-br (right-branch mobile)))
+    (if (null? mobile)
+       0
+       (+ (* -1
+            (if (pair? (branch-structure left-br))
+               (* (branch-length left-br) (rotational-moment (branch-structure left-br)))
+               (* (branch-structure left-br) (branch-length left-br))))
+         (if (pair?  (branch-structure right-br))
+            (* (branch-length right-br) (rotational-moment (branch-structure right-br)))
+            (* (branch-structure right-br) (branch-length right-br)))))))
+
+(define (balance? mobile)
+  (= 0 (rotational-moment mobile)))
+
 (display "c=") c
 (display "total-length=") (total-length c)
 (display "total-weight=") (total-weight c)
+(display "rotational-moment=") (rotational-moment c)
 (newline)
 (display "b=") b
+(display "              /\\") (newline)
+(display "          2/    \\2") (newline)
+(display "        5/\\1 3/\\3") (newline)
+(display "         1 5  2 5") (newline)
 (display "total-length=") (total-length b)
 (display "total-weight=") (total-weight b)
+(display "rotational-moment=") (rotational-moment b)
 (newline)
 (display "a=") a
 (display "total-length=") (total-length a)
 (display "total-weight=") (total-weight a)
+(display "rotational-moment=") (rotational-moment a)
+(display "balance?=") (balance? a)
+
+(display "balance-mobile=") balance-mobile
+(display "total-length=") (total-length balance-mobile)
+(display "total-weight=") (total-weight balance-mobile)
+(display "rotational-moment=") (rotational-moment balance-mobile)
+(display "balance?=") (balance? balance-mobile)
