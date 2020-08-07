d9a5effab4ddcbbafcf88e4f156f215bf9b914c3
birdflu <birdflu@mail.ru>
2020-08-07 19:31:50 +0300

    Update exercise2.37.scm

--- Chapter2/exercise2.37.scm
+++ Chapter2/exercise2.37.scm
@@ -1,13 +1,15 @@
-(include "2.2.3.1.rkt")
+;#lang scheme
 (include "2.36.rkt")
-
-(define m (list (list 1 2 3 4) (list 4 5 6 6) (list 6 7 8 9)))
-
-(define v (list 1 1 5))
-(define w (list 2 2 10))
 
 (define (dot-product v w)
   (accumulate + 0 (map * v w)))
 
 (define (matrix-*-vector m v)
-  (map (λ (x) (accumulate + 0 x)) m))
+  (map (λ (x) (dot-product x v)) m))
+
+(define (transpose mat)
+  (accumulate-n cons nil mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (λ (x) (matrix-*-vector cols x)) m)))
