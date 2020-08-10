67efb87fb007c79b1125a1cb5ca604f692e8d1c4
birdflu <birdflu@mail.ru>
2020-08-10 20:06:12 +0300

    Update exercise2.2.3.2.scm

--- Chapter2/exercise2.2.3.2.scm
+++ Chapter2/exercise2.2.3.2.scm
@@ -1,8 +1,18 @@
+;#lang scheme
+
+; see 2.2.3.2-test.scm
+
 ; Given a positive integer n, find all ordered pairs of distinct positive
 ; integers i and j, where 1 < j < i < n
 
-(include "../utils.scm")
+; Then filter this sequence of pairs to find those whose sum is prime. The
+; filter predicate is called for each element of the sequence; its argument is
+; a pair and it must extract the integers from the pair. 
 
+(include "../utils.scm")
+(include "../Chapter1/1.22.rkt")
+
+; generate all pairs
 (define (generate-pairs init n)
   (accumulate
    append nil (map (λ (i)
@@ -23,3 +33,8 @@
              (map (λ (j) (list i j))
                  (enumerate-interval init (- i 1))))
           (enumerate-interval init n)))
+
+; predicate for primed filter 
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
