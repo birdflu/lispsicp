54e5ecd497aa0ace9775bfc8385326c1def633de
birdflu <birdflu@mail.ru>
2020-08-11 13:51:45 +0300

    Update 2.2.3.2-test.scm

--- Chapter2/tests/2.2.3.2-test.scm
+++ Chapter2/tests/2.2.3.2-test.scm
@@ -29,10 +29,18 @@
                     (list 6 1)
                     (list 6 5)))
 
+(define result (list
+                (list 2 1 3)
+                (list 3 2 5)
+                (list 4 1 5)
+                (list 4 3 7)
+                (list 5 2 7)
+                (list 6 1 7)
+                (list 6 5 11)))
+
   (check-equal? (sort (generate-pairs 1 6) list-less-than?) list1)
   (check-equal? (sort (generate-pairs-2 1 6) list-less-than?) list1)
   (check-equal? (sort (generate-pairs-3 1 6) list-less-than?) list1)
   (check-equal? (sort (filter prime-sum? (generate-pairs-3 1 6)) list-less-than?) prime-list)
-  
-
-  
+  (check-equal? (sort (prime-sum-pairs 6) list-less-than?) result)
+ 
