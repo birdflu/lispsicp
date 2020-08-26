533ae7e6f2105bacd856ebb55b0391a8561c9315
birdflu <birdflu@mail.ru>
2020-08-26 20:50:33 +0300

    add 2.50

---
+++ Chapter2/exercise2.50.scm
@@ -0,0 +1,22 @@
+;#lang scheme
+(include "exercise2.2.5.scm")
+
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate180 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate270 painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+
---
+++ Chapter2/tests/2.50-test.scm
@@ -0,0 +1,8 @@
+#lang scheme
+
+(include "../exercise2.50.scm")
+
+(paint wave)
+(paint (flip-horiz wave ))
+(paint (rotate180 wave ))
+(paint (rotate270 wave ))
