7fb6af8b9cf799289eea21ad40770e5637120159
birdflu <birdflu@mail.ru>
2020-09-28 15:47:46 +0300

    modify 2.58util

--- Chapter2/exercise2.58util.scm
+++ Chapter2/exercise2.58util.scm
@@ -1,53 +1,50 @@
+(define s0 '((x * y) * (x + 3)))
 
-(define s '(x + (3 * (x + (y + 2)))))
+(define s1 '(x + (3 * (x + (y + 2)))))
 
-(define a '(x + 3 * (x + y + 2)))
+(define s2 '(x + 3 * (x + y + 2)))
 
-(define b '(x * y * z + w * q ))
+(define s3 '(x * y * z + w * q ))
 
-(define d '(x * y * z + w * q + r * p * t + n + m))
-;(check-equal? (deriv '(x + (3 * (x + (y + 2)))) 'x)
-;              (prefix-deriv '(+ x (* 3 (+ x (+ y 2)))) 'x))
+(define s4 '(x * y * z + w * q + r * p * t + n + m))
 
-(define (infix-to-prefix exp)
-   (if (list? (caddr exp))
-      (append (cons (cadr exp) 
-                    (if (pair?  exp)
-                        (list (infix-to-prefix (car exp)))
-                        (list (car exp))))
-              (list (infix-to-prefix (caddr exp))))
-      (list (cadr exp) (car exp) (caddr exp))))
+(define s5 '(x * (y + (z + t * (l + m)))))
 
-(define (to-prefix exp)
-  (if (eq? (car exp) '+) 
-          (prefix-infix-to-prefix exp)
-          (make-prefix exp '* 15)))
+(define s6 '(((x + b) + y) + c + (q + a)))
 
+(define s7 '(((x + b) * y) + c * (q * a) + d))
 
-(define (prefix-infix-to-prefix exp)
-   (display exp)
-   (newline)
-  (let ((left (cadr exp))
-        (right (caddr exp)))
-    (list '+ 
-      (if (pair? left)
-        (to-prefix left)
-        left)
-      (if (pair? right)
-        (to-prefix right)
-        right)
-        )))
+(define a1 '(5 + 6 * 6 + 2 * (4 * 1 + 1 * (0 + 0)))) ; 49
 
-(define (make-prefix exp op count)
-  (if (= 0 count) exp
-    (let ((e (epsilon exp op)))
-      (let ((left (car e))
-            (right (cadr e)))
-        (if (null? right)
+(define a2 '(2 * (4 * 1 + 1 * (0 + 0)))) ; 8
+
+(define a3 '(2 + 2 * (3 + 1) * 3 + (2 * (3 + 1) * 2 + 0) * 2)) ; 2 + 24  + 32 = 58
+
+;(define (infix-to-prefix exp)
+;   (if (list? (caddr exp))
+;      (append (cons (cadr exp) 
+;                    (if (pair?  exp)
+;                        (list (infix-to-prefix (car exp)))
+;                        (list (car exp))))
+;              (list (infix-to-prefix (caddr exp))))
+;      (list (cadr exp) (car exp) (caddr exp))))
+
+(define (change-operation op)
+ (if (eq? op '*) '+ '*))
+
+(define (infix-to-prefix exp op)
+;  (display (list (list "exp=" exp) (list "op=" op) (list "count= "  count)))
+;  (newline)
+  (let ((e (epsilon exp op)))
+    (let ((left (car e))
+          (right (cadr e)))
+      (if (null? right)
+        (if (not (pair? left)) 
           left
-          (list op 
-                (make-prefix left op (- count 1)) 
-                (make-prefix right op (- count 1))))))))
+          (infix-to-prefix left (change-operation op)))
+        (list op 
+              (infix-to-prefix left '+) 
+              (infix-to-prefix right '+))))))
 
 (define (epsilon exp op)
   (ambit (list '() exp) op)) 
@@ -57,17 +54,16 @@
   ;(newline)
   (let ((exp (cadr s))
        (left (car s)))
-    (if (pair? exp)
+     (if (pair? exp)
         (if (null? (cdr exp))
           (list (append left exp) '())
           (if (eq? op (cadr exp))
             (list (append left 
                         (if (null? left) (car exp) (list (car exp)))) 
-                  (if (null? (cdddr exp)) (caddr exp) (cddr exp))
-                  )
-  ;          (list (append left (list (car exp))) (cddr exp))
+                  (if (null? (cdddr exp)) (caddr exp) (cddr exp)))
             (ambit (list (append left (list (car exp))) (cdr exp)) op)))
-        (list (append left exp) '()))
+        (list (append left exp) '()))))
 
-    ))
+
+
 
