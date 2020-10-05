c619984bcad73d996d9c2314faf3028ce8fcb83c
birdflu <birdflu@mail.ru>
2020-10-05 17:02:58 +0300

    permutation. part 4

--- Draft/permutations.scm
+++ Draft/permutations.scm
@@ -13,26 +13,38 @@
 ; (231)()
 
 (define (permutations result source tail)
-; (let ((tail (remove (car source)  source)))
-  (let ((tail (append (cdr source) (list (car source)))))
-  (display "result:") (display result)
-  (display ", source:") (display source)
-  (display ", tail:") (display tail)
+  (display (list " input :" result source tail))
   (newline)
-     (if (null? source)
-      result
-      (let ((permutation (list (append result (list (car source))) tail)))
-        (if (null? (cdr source))
-          (list permutation)
-          (cons permutation 
-              (permutations result (cdr source) (append tail (car source)))))))))
+  (if (null? tail)
+      '();result
+      (let ((tail (append (cdr tail) (list (car tail))))
+            (item (list (car source))))
+        (if (null? source)
+            ;(append result (permutations result tail tail))
+            result
+            (let ((permutation (list (append result item) 
+                                     source 
+                                     (remove-sublist item tail)))
+                  )
+              (display "result:") (display result)
+              (display ", source:") (display source)
+              (display ", tail:") (display tail)
+              (display ", permutation:") (display permutation)
+              (newline)
+              (if (null? (cdr source))
+                  (cons permutation 
+                        (permutations (car permutation) 
+                                      (caddr permutation) 
+                                      (caddr permutation)));(list permutation)
+                  (cons permutation 
+                        (permutations result (cdr source) tail))))))))
 
 (define (remove-sublist sublist list)
   (if (null? sublist)
       list
       (remove-sublist (remove (car sublist) sublist) (remove (car sublist) list))))
 
-(permutations '(2) '(1 3) '())
-(permutations '() '(1 2 3) '())
-
-
+(permutations '(2) '(1 3) '(1 3))
+;(permutations '() '(1 2 3) '(1 2 3))
+;(permutations '(2 3) '(1) '(1))
+;(permutations '(2 3 1) '() '())
