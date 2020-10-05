5ec2d58f708858fdbf5ae0f7f505aaa9850607d3
birdflu <birdflu@mail.ru>
2020-10-05 07:27:08 +0300

    permutation. part 3

--- Draft/permutations.scm
+++ Draft/permutations.scm
@@ -12,29 +12,27 @@
 ; (23)(1) ->
 ; (231)()
 
-(define (permutations result source)
+(define (permutations result source tail)
+; (let ((tail (remove (car source)  source)))
+  (let ((tail (append (cdr source) (list (car source)))))
   (display "result:") (display result)
   (display ", source:") (display source)
+  (display ", tail:") (display tail)
   (newline)
-  (if (null? source)
+     (if (null? source)
       result
-      (let ((permutation (append result (list (car source)))))
+      (let ((permutation (list (append result (list (car source))) tail)))
         (if (null? (cdr source))
           (list permutation)
           (cons permutation 
-              (permutations result (cdr source)))))))
+              (permutations result (cdr source) (append tail (car source)))))))))
 
-;(permutations '(8) '())
-;(permutations '() '(1 2 3 4))
-;(permutations '(8) '(1 2 3 4))
-;(permutations '(2) '(1 3 4))
-;(permutations '(2 3 4)  '(1))
- 
-(define (expand items)
-  (let ((result (car items))
-        (source (cadr items)))
-  (append (permutations result source)) 
-  ))
+(define (remove-sublist sublist list)
+  (if (null? sublist)
+      list
+      (remove-sublist (remove (car sublist) sublist) (remove (car sublist) list))))
 
-(expand '(() (1 2 3)))
-;(expand '((1) (2 3)) 2)
+(permutations '(2) '(1 3) '())
+(permutations '() '(1 2 3) '())
+
+
