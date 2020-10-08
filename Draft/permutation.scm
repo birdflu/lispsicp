5f1050bb0b0014e54b3f4dd4d11b2a10c3c2ff54
birdflu <birdflu@mail.ru>
2020-10-08 23:24:48 +0300

    delete old version and pull release

--- Draft/permutation.scm
+++ Draft/permutation.scm
@@ -12,28 +12,11 @@
 ; (23)(1) ->
 ; (231)()
 
-;(define (perm first second constant tail)
-;;  (display first) (display second) (newline)
-;  (let ((null-first? (null? first))
-;        (null-second? (null? second)))
-;  (cond [(and null-first? null-second?) '()]
-;        [(and null-second? (not null-first?))
-;         (perm  (cdr first) constant constant tail)]
-;        [(and (not null-second?) null-first?)
-;         '()]
-;;          (cons (list (car second)) (perm first (cdr second) constant)) ]
-;        [else 
-;          (append 
-;            (list (cons (car first) (list (car second)))) 
-;            (perm first (cdr second) constant tail))]
-;        )))
-;
-
 (define (perm first second constant)
 ;  (display first) (display second) (newline)
   (let ((null-first? (null? first))
         (null-second? (null? second)))
-  (cond [(and null-second? (not null-first?)) '()]
+  (cond [null-second? '()]
         [else 
           (append 
             (list (append first (list (car second)))) 
@@ -55,15 +38,28 @@
            (perm result tail tail))))
   
 (define (permutation items)
-  (let ((r (permutate items)))
-    (display "=>") (display r) (newline)
-    (map permutate r)))
+  (if (null? items)
+      '()
+    (append items (permutation (flatmap permutate items)))))
 
         
 ;(perm  '() '(1 2 3) '(1 2 3))
 ;(perm  '(2) '(1 3) '(1 3))
-(permutate  '(( 2 ) ( 1 3 )))
-(permutate  '(( 2 1 ) ( 3 )))
+;(permutate  '(( 2 ) ( 1 3 )))
+;(permutate  '(( 2 1 ) ( 3 )))
 ;(perm '(2 1) '(3) '(3))
-(permutation  '((2) (1 3)))
+;(permutation  '(() (1 2)))
+;(permutation  '(() (1 2 3)))
+;(permutate  '(() (1 2 3)))
+;(flatmap permutate (permutate  '(() (1 2 3))))
+;(flatmap permutate (flatmap permutate (permutate  '(() (1 2 3)))))
+;(flatmap permutate (flatmap permutate (flatmap permutate (permutate  '(() (1 2 3))))))
+;(permutation (permutate '(() (1 2 3))))
+(permutation '((() (1 2 3))))
+
+
+
+
+
+
 
