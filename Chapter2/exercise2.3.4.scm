b47fdcad8fa2bb93e0d4fe954868921fec901431
birdflu <birdflu@mail.ru>
2020-11-26 14:53:37 +0300

    2.3.4.scm

--- Chapter2/exercise2.3.4.scm
+++ Chapter2/exercise2.3.4.scm
@@ -14,6 +14,8 @@
 
 (define (weight-leaf x) (caddr x))
 
+; > (make-code-tree (make-leaf 'C 1) (make-leaf 'D 1))
+; '((leaf C 1) (leaf D 1) (C D) 2)
 (define (make-code-tree left right)
   (list left
         right
@@ -34,6 +36,8 @@
       (weight-leaf tree)
       (cadddr tree)))
 
+; The following procedure implements the decoding algorithm. It takes
+; as arguments a list of zeros and ones, together with a Huffman tree.
 (define (decode bits tree)
   (define (decode-1 bits current-branch)
     (if (null? bits)
@@ -45,4 +49,43 @@
                     (decode-1 (cdr bits) tree))
               (decode-1 (cdr bits) next-branch)))))
   (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit: CHOOSE-BRANCH" bit))))
+
+
+; adjoin element into list, saving its increasing order of weight. 
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+; The following procedure takes a list of symbol-frequency pairs such as
+; ((A 4) (B 2) (C 1) (D 1)) and constructs an initial ordered set of
+; leaves, ready to be merged according to the Huffman algorithm:
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair)   ; symbol
+                               (cadr pair)) ; frequency
+                    (make-leaf-set (cdr pairs))))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
